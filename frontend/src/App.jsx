6f7bc0aed900c1c38ba350b85ff571045a63d95b
Bijon2002 <bijosilin02@gmail.com>
2026-03-05 20:49:20 +0530

    Improve UI layout and navigation styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,52 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
-import Game from "./pages/Game";
-import Leaderboard from "./pages/Leaderboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-import History from "./pages/History";
-// import ProtectedRoute from "./components/ProtectedRoute";
-
+import Game from "./pages/Game.jsx";
+import History from "./pages/History.jsx";
+import Leaderboard from "./pages/Leaderboard.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        {/* Default */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Public routes */}
         <Route path="/login" element={<Login />} />
-        { <Route path="/register" element={<Register />} /> }
-     <Route
-  path="/game"
-  element={
-    <ProtectedRoute>
-      <Game />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/history"
-  element={
-    <ProtectedRoute>
-      <History />
-    </ProtectedRoute>
-  }
-/>
+        <Route path="/register" element={<Register />} />
 
-<Route path="/leaderboard" element={<Leaderboard />} />
+        {/* Protected routes */}
+        <Route
+          path="/game"
+          element={
+            <ProtectedRoute>
+              <Game />
+            </ProtectedRoute>
+          }
+        />
 
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <History />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Public leaderboard (can make it protected if you want) */}
+        <Route path="/leaderboard" element={<Leaderboard />} />
+
+        {/* Fallback */}
         <Route path="*" element={<div style={{ padding: 40 }}>Not found</div>} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-{/* <Route
-  path="/history"
-  element={
-    <ProtectedRoute>
-      <History />
-    </ProtectedRoute>
-  }
-/> */}
